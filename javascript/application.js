5f60b655d87dac54a7f85e0da31311fcde53defb
Harper Bellows <harper.bellows@gmail.com>
2019-01-20 12:03:47 -0700

    add possible fix to game logic

--- javascript/application.js
+++ javascript/application.js
@@ -1,26 +1,27 @@
-let randomNumber = Math.floor((Math.random() * 100) + 1);
 const guessSubmit = document.querySelector('.guessSubmit');
 const mostRecentGuess = document.querySelector('.mostRecentGuess');
 const correctGuess = document.querySelector('.correctGuess');
-const userGuess = document.querySelector('.userGuess');
+const guessField = document.querySelector('.userGuess');
 const highLow = document.querySelector('.highLow');
 
-userGuess.focus();
+guessField.focus();
 
 guessSubmit.addEventListener('click', guessCheck);
 
 function guessCheck() {
-  mostRecentGuess.textContent = 'Your Most Recent Guess: ' + userGuess.value;
-  if (Number(userGuess.value) === randomNumber) {
+  let userGuess = Number(guessField.value);
+  mostRecentGuess.textContent = 'Your Most Recent Guess: ' + userGuess;
+  if (userGuess === randomNumber) {
     correctGuess.textContent = "BOOM!";
     guessSubmit.disabled = true;
-    userGuess.disabled = true;
+    guessField.disabled = true;
     resetGame();
-  } else if (Number(userGuess.value) > randomNumber) {
+  } else if (userGuess > randomNumber) {
     highLow.textContent = "That is too high!";
-    userGuess.focus();
-  } else if ((Number(userGuess.value) < randomNumber)) {
-    highlow.textContent = "That is too low!";
-    userGuess.focus();
+  } else if (userGuess < randomNumber) {
+    highLow.textContent = "That is too low!";
   }
-}
+  
+  guessField.value = '';
+  guessField.focus();
+};
