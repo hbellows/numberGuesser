f77e6d587060c0718b82b0bb50a99fd6b4ebc0f8
Harper Bellows <harper.bellows@gmail.com>
2019-01-20 12:35:44 -0700

    wip -> still trying to nail down game logic

--- javascript/application.js
+++ javascript/application.js
@@ -10,19 +10,37 @@ submitGuess.addEventListener('click', guessCheck);
 
 function guessCheck() {
   let userGuess = Number(guessField.value);
-  mostRecentGuess.textContent = 'Your Most Recent Guess: ' + userGuess;
-  if (userGuess === randomNumber) {
-    highLow.textContent = '';
-    correctGuess.textContent = "BOOM!";
-    submitGuess.disabled = true;
-    guessField.disabled = true;
-    resetGame();
-  } else if (userGuess > randomNumber) {
-    highLow.textContent = "That is too high";
-  } else if (userGuess < randomNumber) {
-    highLow.textContent = "That is too low";
-  }
+  if (validateGuess(userGuess)) {
+    mostRecentGuess.textContent = 'Your Most Recent Guess: ' + userGuess;
 
-  guessField.value = '';
-  guessField.focus();
+    if (userGuess === randomNumber) {
+      highLow.textContent = '';
+      correctGuess.textContent = "BOOM!";
+      guessSubmit.disabled = true;
+      guessField.disabled = true;
+      resetGame();
+    } else if (userGuess > randomNumber) {
+      highLow.textContent = "That is too high!";
+    } else if (userGuess < randomNumber) {
+      highLow.textContent = "That is too low!";
+    }
+
+    guessField.value = '';
+    guessField.focus();
+  };
 };
+
+const guessError = document.querySelector('.error');
+
+function validateGuess(guess) {
+  guessError.textContent = '';
+  if (isNaN(guess)) {
+    guessError.textContent = "You need to guess a numberical number.";
+    return false;
+  } else if (guess < min || guess > max) {
+    guessError.textContent = 'Your guess should be between ' + min + ' and ' + max;
+    return false;
+  } else {
+    return true;
+  }
+}
